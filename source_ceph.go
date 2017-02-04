db88b359223d206e6175efe93998d904fde88aa2
Hieu Tran Trung <hieutrantrung@chotot.vn>
2017-02-04 12:05:26 +0700

    perform image operator integrate with Ceph

---
+++ source_ceph.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/noahdesu/go-ceph/rados"
+)
+
+const ImageSourceTypeCeph ImageSourceType = "ceph"
+const PoolName string = "media"
+
+type CephObject struct {
+	NameSpace string
+	OID       string
+}
+
+type CephImageSource struct {
+	Config     *SourceConfig
+	Connection *rados.Conn
+}
+
+func NewCephImageSource(config *SourceConfig) ImageSource {
+	CISource := &CephImageSource{}
+	CISource.Config = config
+	CISource.Connection = MakeConnection()
+	return CISource
+}
+
+func MakeConnection() *rados.Conn {
+	conn, err := rados.NewConn()
+	if err != nil {
+		panic(err)
+	}
+	conn.ReadConfigFile("/etc/ceph/ceph.conf")
+	err = conn.Connect()
+	if err != nil {
+		panic(err)
+	}
+	return conn
+}
+
+func (s *CephImageSource) Matches(r *http.Request) bool {
+	return r.Method == "GET" && r.URL.Query().Get("cns") != "" && r.URL.Query().Get("cid") != ""
+}
+
+func (s *CephImageSource) GetImage(req *http.Request) ([]byte, error) {
+	co := parseObj(req)
+	buf, err := s.fetchObject(co)
+	if err != nil {
+		return nil, err
+	}
+	return buf, nil
+}
+
+func parseObj(req *http.Request) CephObject {
+	ns := req.URL.Query().Get("cns")
+	id := req.URL.Query().Get("cid")
+	return CephObject{ns, id}
+}
+
+func (s *CephImageSource) fetchObject(co CephObject) ([]byte, error) {
+	ioctx, err := s.Connection.OpenIOContext(PoolName)
+	defer ioctx.Destroy()
+	if err != nil {
+		return nil, err
+	}
+	buf := make([]byte, 1048676)
+	_, err = ioctx.GetXattr(co.NameSpace, co.OID, buf)
+	if err != nil {
+		return nil, err
+	}
+	return buf, nil
+}
+
+func init() {
+	RegisterSource(ImageSourceTypeCeph, NewCephImageSource)
+}
