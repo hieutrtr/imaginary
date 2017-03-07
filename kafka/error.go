7edf4ad4bc4630e1606429e948bbdc68ac1bd8c0
Hieu Tran Trung <hieutrantrung@chotot.vn>
2017-03-07 15:55:14 +0700

    correct error

--- kafka/error.go
+++ kafka/error.go
@@ -20,16 +20,16 @@ var (
 	ErrProducer = NewError("Producer is not ready", InternalError)
 )
 
-// NewError create new error
-func NewError(err string, code uint8) Error {
-	err = strings.Replace(err, "\n", "", -1)
-	return EventError{err, code}
-}
-
 // EventError Kafka error handling
 type EventError struct {
 	Message string `json:"message"`
 	Code    uint8  `json:"code"`
+}
+
+// NewError create new error
+func NewError(err string, code uint8) EventError {
+	err = strings.Replace(err, "\n", "", -1)
+	return EventError{err, code}
 }
 
 func (e EventError) JSON() []byte {
