020ac7bed77e7d5364c6d51e8aa8b9cbd04e3f33
Hieu Tran Trung <hieutrantrung@chotot.vn>
2017-03-07 15:29:30 +0700

    kafka error handling

--- kafka/error.go
+++ kafka/error.go
@@ -1,6 +1,30 @@
 package kafka
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"strings"
+)
+
+const (
+	Unavailable uint8 = iota
+	BadRequest
+	NotAllowed
+	Unsupported
+	Unauthorized
+	InternalError
+	NotFound
+)
+
+var (
+	ErrMessage  = NewError("Invailid or missing message", BadRequest)
+	ErrProducer = NewError("Producer is not ready", InternalError)
+)
+
+// NewError create new error
+func NewError(err string, code uint8) Error {
+	err = strings.Replace(err, "\n", "", -1)
+	return EventError{err, code}
+}
 
 // EventError Kafka error handling
 type EventError struct {
